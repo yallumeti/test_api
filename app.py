da2901eff0bd342926edfe41bcfa8719a95272ff
yallumeti <yallumeti473@gmail.com>
2026-04-01 10:13:00 +0530

    Add Flask routes for posts, comments, and albums

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+from flask import Flask, jsonify
+from flask import render_template
+import requests
 
+app = Flask(__name__)
+
+@app.route("/posts")
+def posts():
+    return jsonify(requests.get("https://jsonplaceholder.typicode.com/posts").json())
+
+@app.route("/comments")
+def comments():
+    return jsonify(requests.get("https://jsonplaceholder.typicode.com/comments").json())
+
+@app.route("/albums")
+def albums():
+    return jsonify(requests.get("https://jsonplaceholder.typicode.com/albums").json())
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
